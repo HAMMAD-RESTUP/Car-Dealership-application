ccc3b76b29eb1bc02dd893e5da4e20646fb7c62c
HAMMAD-RESTUP <hammad307dofficial@gmail.com>
2026-09-21 09:42:00 +0500

    car listing

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import BrandMarquee from "@/components/Sections/brandmarquee";
 import CarListing from "@/components/Sections/carListing";
 import Footer from "@/components/Layout/Footer/footer";
 import WhyChooseUs from "@/components/Sections/whychooseus";
-import VideoSection from "@/components/Sections/videosection";
 import Testimonials from "@/components/Sections/testimonials";
 import VehicleSearchBar from "@/components/Sections/VehicleSearchBar";
 import Welcome from "@/components/Sections/Welcome";
